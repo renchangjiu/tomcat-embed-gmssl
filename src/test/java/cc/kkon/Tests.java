32fe7551c1193f69b00bef090ab90ab02973cadd
yui <cc@kkon.cc>
2024-03-28 09:41:57 +0800

    upgrade gmssl_provider version to 2024

--- src/test/java/cc/kkon/Tests.java
+++ src/test/java/cc/kkon/Tests.java
@@ -2,7 +2,6 @@ package cc.kkon;
 
 import cc.kkon.utils.Tomcats;
 import org.apache.catalina.startup.Tomcat;
-import org.junit.Test;
 
 /**
  * @author yui
@@ -10,7 +9,7 @@ import org.junit.Test;
 public class Tests {
 
 
-    @Test
+    // @Test
     public void listen() throws Exception {
         int port = 0;
         String certPassword = "12345678";
@@ -20,6 +19,8 @@ public class Tests {
         Tomcats.addServlet(tomcat, new EchoServlet());
         tomcat.start();
         // 阻塞
-        // tomcat.getServer().await();
+        tomcat.getServer().await();
     }
+
+
 }
